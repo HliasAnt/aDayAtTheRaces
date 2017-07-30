579b7e0c3fafa563074fc83d72a0d04eb943b8ca
hlias antonopoulos <hlias_antonopoulos@hotmail.com>
2017-07-30 15:26:20 +0300

    Add project files.

---
+++ aDayAtTheRaces/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace aDayAtTheRaces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioHlias = new System.Windows.Forms.RadioButton();
+            this.radioXara = new System.Windows.Forms.RadioButton();
+            this.radioHra = new System.Windows.Forms.RadioButton();
+            this.MinBetLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHlias = new System.Windows.Forms.TextBox();
+            this.textBoxXara = new System.Windows.Forms.TextBox();
+            this.textBoxHra = new System.Windows.Forms.TextBox();
+            this.BetPlayerName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDownBets = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDownDog = new System.Windows.Forms.NumericUpDown();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.BetButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1091, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(16, 28);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(93, 26);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(16, 91);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(93, 26);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(16, 158);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(93, 26);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(16, 214);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(93, 26);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 293);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Betting";
+            // 
+            // radioHlias
+            // 
+            this.radioHlias.AutoSize = true;
+            this.radioHlias.Location = new System.Drawing.Point(16, 373);
+            this.radioHlias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioHlias.Name = "radioHlias";
+            this.radioHlias.Size = new System.Drawing.Size(68, 21);
+            this.radioHlias.TabIndex = 6;
+            this.radioHlias.Text = "HLIAS";
+            this.radioHlias.UseVisualStyleBackColor = true;
+            this.radioHlias.CheckedChanged += new System.EventHandler(this.radioHlias_CheckedChanged);
+            // 
+            // radioXara
+            // 
+            this.radioXara.AutoSize = true;
+            this.radioXara.Location = new System.Drawing.Point(16, 414);
+            this.radioXara.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioXara.Name = "radioXara";
+            this.radioXara.Size = new System.Drawing.Size(66, 21);
+            this.radioXara.TabIndex = 7;
+            this.radioXara.Text = "XARA";
+            this.radioXara.UseVisualStyleBackColor = true;
+            this.radioXara.CheckedChanged += new System.EventHandler(this.radioXara_CheckedChanged);
+            // 
+            // radioHra
+            // 
+            this.radioHra.AutoSize = true;
+            this.radioHra.Location = new System.Drawing.Point(16, 457);
+            this.radioHra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioHra.Name = "radioHra";
+            this.radioHra.Size = new System.Drawing.Size(58, 21);
+            this.radioHra.TabIndex = 8;
+            this.radioHra.Text = "HRA";
+            this.radioHra.UseVisualStyleBackColor = true;
+            this.radioHra.CheckedChanged += new System.EventHandler(this.radioHra_CheckedChanged);
+            // 
+            // MinBetLabel
+            // 
+            this.MinBetLabel.AutoSize = true;
+            this.MinBetLabel.Location = new System.Drawing.Point(21, 337);
+            this.MinBetLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MinBetLabel.Name = "MinBetLabel";
+            this.MinBetLabel.Size = new System.Drawing.Size(76, 17);
+            this.MinBetLabel.TabIndex = 9;
+            this.MinBetLabel.Text = "MiimumBet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 336);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Bets";
+            // 
+            // textBoxHlias
+            // 
+            this.textBoxHlias.Location = new System.Drawing.Point(233, 369);
+            this.textBoxHlias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxHlias.Name = "textBoxHlias";
+            this.textBoxHlias.Size = new System.Drawing.Size(273, 22);
+            this.textBoxHlias.TabIndex = 11;
+            // 
+            // textBoxXara
+            // 
+            this.textBoxXara.Location = new System.Drawing.Point(233, 412);
+            this.textBoxXara.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxXara.Name = "textBoxXara";
+            this.textBoxXara.Size = new System.Drawing.Size(273, 22);
+            this.textBoxXara.TabIndex = 12;
+            // 
+            // textBoxHra
+            // 
+            this.textBoxHra.Location = new System.Drawing.Point(233, 457);
+            this.textBoxHra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxHra.Name = "textBoxHra";
+            this.textBoxHra.Size = new System.Drawing.Size(273, 22);
+            this.textBoxHra.TabIndex = 13;
+            // 
+            // BetPlayerName
+            // 
+            this.BetPlayerName.AutoSize = true;
+            this.BetPlayerName.Location = new System.Drawing.Point(25, 571);
+            this.BetPlayerName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BetPlayerName.Name = "BetPlayerName";
+            this.BetPlayerName.Size = new System.Drawing.Size(45, 17);
+            this.BetPlayerName.TabIndex = 14;
+            this.BetPlayerName.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 571);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Bets";
+            // 
+            // numericUpDownBets
+            // 
+            this.numericUpDownBets.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownBets.Location = new System.Drawing.Point(149, 569);
+            this.numericUpDownBets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownBets.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDownBets.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownBets.Name = "numericUpDownBets";
+            this.numericUpDownBets.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDownBets.TabIndex = 16;
+            this.numericUpDownBets.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(241, 571);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Dog";
+            // 
+            // numericUpDownDog
+            // 
+            this.numericUpDownDog.Location = new System.Drawing.Point(316, 569);
+            this.numericUpDownDog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownDog.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDownDog.Name = "numericUpDownDog";
+            this.numericUpDownDog.Size = new System.Drawing.Size(59, 22);
+            this.numericUpDownDog.TabIndex = 18;
+            this.numericUpDownDog.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(809, 359);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(175, 118);
+            this.StartButton.TabIndex = 19;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // BetButton
+            // 
+            this.BetButton.Location = new System.Drawing.Point(444, 569);
+            this.BetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BetButton.Name = "BetButton";
+            this.BetButton.Size = new System.Drawing.Size(121, 28);
+            this.BetButton.TabIndex = 20;
+            this.BetButton.Text = "Bet";
+            this.BetButton.UseVisualStyleBackColor = true;
+            this.BetButton.Click += new System.EventHandler(this.BetButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 604);
+            this.Controls.Add(this.BetButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.numericUpDownDog);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDownBets);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BetPlayerName);
+            this.Controls.Add(this.textBoxHra);
+            this.Controls.Add(this.textBoxXara);
+            this.Controls.Add(this.textBoxHlias);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MinBetLabel);
+            this.Controls.Add(this.radioHra);
+            this.Controls.Add(this.radioXara);
+            this.Controls.Add(this.radioHlias);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Race Betting Control";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioHlias;
+        private System.Windows.Forms.RadioButton radioXara;
+        private System.Windows.Forms.RadioButton radioHra;
+        private System.Windows.Forms.Label MinBetLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHlias;
+        private System.Windows.Forms.TextBox textBoxXara;
+        private System.Windows.Forms.TextBox textBoxHra;
+        private System.Windows.Forms.Label BetPlayerName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDownBets;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDownDog;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button BetButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ aDayAtTheRaces/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aDayAtTheRaces
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            SetStartingRadio(); //players infos
+            UpdateTheRadios();  //info radios texts
+            SetDogsValues();    //dogs infos
+
+        }
+
+        // Panel code
+
+        Players[] RacePlayers = new Players[3]; //create the betplayers
+        Bets[] MyBets = new Bets[3];            // create the bets of the players
+        
+        
+        public void SetStartingRadio() // create players bets objects
+        {
+            RacePlayers[0] = new Players() { Name = "HLIAS", Cash = 100, MyRadioButton = radioHlias, MyTextBox = textBoxHlias };
+            RacePlayers[1] = new Players() { Name = "XARA", Cash = 85, MyRadioButton = radioXara,MyTextBox=textBoxXara };
+            RacePlayers[2] = new Players() { Name = "HRA", Cash = 75, MyRadioButton = radioHra,MyTextBox=textBoxHra };
+            MyBets[0] = new Bets();
+            MyBets[1] = new Bets();
+            MyBets[2] = new Bets();
+        }
+        public void UpdateTheRadios()  // update the radios text
+        {
+            RacePlayers[0].UpdateRadio();
+            RacePlayers[1].UpdateRadio();
+            RacePlayers[2].UpdateRadio();
+            
+        }
+
+        private void radioHlias_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioHlias.Enabled==true)
+            {
+                BetPlayerName.Text = RacePlayers[0].Name; //update the player bet label depends the selected radio
+            }
+        }
+
+        private void radioXara_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioXara.Enabled == true)
+            {
+                BetPlayerName.Text = RacePlayers[1].Name;
+            }
+        }
+
+        private void radioHra_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioHra.Enabled == true)
+            {
+                BetPlayerName.Text = RacePlayers[2].Name;
+            }
+        }
+
+        private void BetButton_Click(object sender, EventArgs e) // place the bet and inform textbox
+        {
+            for (int i = 0; i < 3; i++)
+            {
+
+                if (BetPlayerName.Text == RacePlayers[i].Name)
+                {
+                    RacePlayers[i].MyTextBox.Text = BetPlayerName.Text + " has bet " + numericUpDownBets.Value + " on " +
+                        numericUpDownDog.Value + " Dog";
+                    MyBets[i].StoreBetAmount = (int)numericUpDownBets.Value; 
+                    MyBets[i].StoreDogChoice = (int)numericUpDownDog.Value;
+                }
+
+               
+                
+            }
+
+        }
+
+
+        //Dogs code
+
+        Dogs[] Mydog = new Dogs[4]; // create the 4dogs object
+        Random MyRandomizer = new Random(); // random value of dogs movement
+        
+
+        public void SetDogsValues()
+        {
+
+            Mydog[0] = new Dogs()
+            {
+                MyPictureBox = pictureBox2,
+                StartingPotition = pictureBox2.Left,
+                RacetrackLength = pictureBox1.Width - pictureBox2.Width, Randomizer = MyRandomizer
+            };
+            Mydog[1] = new Dogs()
+            {
+                MyPictureBox = pictureBox3,
+                StartingPotition = pictureBox3.Left,
+                RacetrackLength = pictureBox1.Width - pictureBox3.Width,Randomizer=MyRandomizer
+                
+
+            };
+            Mydog[2] = new Dogs()
+            {
+                MyPictureBox = pictureBox4,
+                StartingPotition = pictureBox4.Left,
+                RacetrackLength = pictureBox1.Width - pictureBox4.Width,Randomizer=MyRandomizer
+                
+
+            };
+            Mydog[3] = new Dogs()
+            {
+                MyPictureBox = pictureBox5,
+                StartingPotition = pictureBox5.Left,
+                RacetrackLength = pictureBox1.Width - pictureBox5.Width,Randomizer=MyRandomizer
+                
+            };
+        }  // set the 4 dogs infos
+
+        private void timer1_Tick(object sender, EventArgs e) //timer for calling the run method when the start button is pressed
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (Mydog[i].Run()==true)
+                {
+                    Mydog[i].Run();
+                    
+                }
+                else
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Dog number #" + i.ToString() + " Won");
+                    int Winner = i;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        RacePlayers[j].Cash = MyBets[j].WinnerPayBack(RacePlayers[j].Cash, MyBets[j].StoreBetAmount, Winner);
+                    }
+                    UpdateTheRadios();
+                    for (int k = 0; k <4; k++)
+                    {
+                        Mydog[k].TakeStartingPotition();
+                    }
+                    break;
+                }
+                
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+           // SetDogsValues();
+            timer1.Start();
+        }
+    }
+
+
+    public class Players
+    {
+        public string Name;
+        public int Cash;
+        public RadioButton MyRadioButton;
+        public Label MyLabel;
+        public TextBox MyTextBox;
+
+
+        public void  UpdateRadio()
+        {
+            MyRadioButton.Text = Name + " has " + Cash + "$";
+          
+        }
+
+    }
+
+    public class Dogs
+    {
+        public int StartingPotition;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int location = 0;
+        public Random Randomizer;
+
+        public bool Run()
+        {
+            
+            location = Randomizer.Next(10, 30);
+            MyPictureBox.Left += location;
+            if (MyPictureBox.Left<=RacetrackLength)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public void TakeStartingPotition()
+        {
+            MyPictureBox.Left = StartingPotition;
+        }
+    }
+
+    public class Bets
+    {
+        
+        public int StoreBetAmount;
+        public int StoreDogChoice;
+        public int WinnerPayBack(int Bcash,int BetAmount,int DogWinner)
+        {
+            if (DogWinner==StoreDogChoice)
+            {
+                Bcash += (BetAmount * 2);
+                return Bcash;
+            }
+            else
+            {
+                Bcash -= BetAmount;
+                return Bcash;
+            }
+            
+        }
+       
+
+
+    }
+}
